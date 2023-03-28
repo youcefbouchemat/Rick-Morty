02957974ec550fc29168ccef68de04e486d7a58c
YoucefMac <youcefbou1998@gmail.com>
2023-03-28 18:19:16 +0100

    creat character service api

---
+++ Base/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Rick&Morty
+//
+//  Created by apple on 28/3/2023.
+//
+
+import Foundation
+
+enum APIConstants{
+    static let baseUrl = "https://rickandmortyapi.com/api/"
+}
---
+++ Service/CharacterService.swift
@@ -0,0 +1,34 @@
+//
+//  CharacterService.swift
+//  Rick&Morty
+//
+//  Created by apple on 28/3/2023.
+//
+
+import Foundation
+
+
+protocol CharacterServiceProtocol{
+    func fetchCharacters() async throws -> [CharacterModel]
+}
+
+final class CharacterService: CharacterServiceProtocol{
+    
+    enum CharacterServiceError: Error{
+        case failed
+        case failedToDecode
+        case invalidStatusCode
+    }
+    
+    func fetchCharacters() async throws -> [CharacterModel] {
+        let urlSession = URLSession.shared
+        let url  = URL(string: APIConstants.baseUrl.appending("character"))
+        let (data,response) = try await urlSession.data(from: url!)
+        
+        guard let response = response as? HTTPURLResponse,
+              response.statusCode == 200 else{
+            throw CharacterServiceError.invalidStatusCode
+        }
+        return try JSONDecoder().decode([CharacterModel].self, from: data)
+    }
+}
