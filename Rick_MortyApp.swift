5d24b726d581eac0f091195f188e3bc29a106549
YoucefMac <youcefbou1998@gmail.com>
2023-03-28 17:37:48 +0100

    initial commit

---
+++ Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by apple on 28/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
