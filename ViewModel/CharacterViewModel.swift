bb4ddce4bc488d2b0a83e3473ecfb533e3e25071
YoucefMac <youcefbou1998@gmail.com>
2023-03-29 08:22:26 +0100

    creat character viewmodel

---
+++ ViewModel/CharacterViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  CharacterViewModel.swift
+//  Rick&Morty
+//
+//  Created by apple on 28/3/2023.
+//
+
+import Foundation
+
+protocol CharacterViewModelProtocol: ObservableObject{
+    func fetchCharacter() async
+}
+
+final class CharacterViewModel: CharacterViewModelProtocol{
+    
+    enum State{
+        case na
+        case loading
+        case success(data: [CharacterModel])
+        case failed(error: Error)
+    }
+    
+    @Published private(set) var state: State = .na
+    
+    private let service: CharacterService
+    
+    init(service: CharacterService) {
+        self.service = service
+    }
+    
+    func fetchCharacter() async {
+        self.state = .loading
+        
+        do{
+            let characters = try await service.fetchCharacters()
+            self.state = .success(data: characters)
+        } catch {
+            self.state = .failed(error: error)
+        }
+    }
+    
+    
+}
